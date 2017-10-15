8c0d701bc9b30cada9fe55e277e48e4894dc15a6
Brandon Nicholls <brandon.nicholls@gmail.com>
2017-10-14 23:24:56 -0600

    Change to other

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 http
   .createServer((req, res) => {
-    res.write('hello world')
+    res.write('hello other world')
     res.end()
   })
   .listen(process.env.PORT || 8000)
